679df1544b5b263ce1a846f7aa1ccf00ada7b203
Amandeep Singh <aman.22275@gmail.com>
2018-12-18 13:02:49 +1100

    Commit All models

---
+++ LibraryData/Models/LibraryBranch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LibraryData.Models
+{
+    public class LibraryBranch
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30, ErrorMessage = "Limit branch to 30 char only")]
+        public string Name { get; set; }
+        [Required]
+        public String Address { get; set; }
+        public string Telephone { get; set; }
+        public String Description { get; set; }
+        public DateTime OpenDate { get; set; }
+        public virtual IEnumerable<Patron> Patrons{ get; set; }
+        public virtual IEnumerable<LibraryAsset> LibraryAssets{ get; set; }
+        public string ImageUrl { get; set; }
+    }
+
+}
---
+++ LibraryData/Models/Patron.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData.Models
+{
+    public class Patron
+    {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public String TelephoneNumber { get; set; }
+
+        //Patrons will have some other objects which used as foreign key. 
+        //Such as Library Card.
+        //##Foreign Key
+        /*
+         * Virtual is used for lazy load that property data.
+        Lazy loading loads a collection from database first time it accessed.
+        */
+
+        public virtual LibraryCard LibraryCard { get; set; }
+        public virtual LibraryBranch HomeLibraryBranch { get; set; }
+    }
+}
